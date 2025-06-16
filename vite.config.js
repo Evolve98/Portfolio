6953b3e330f34a50935e6d8fd78f6ddb8c056b31
Evolve98 <aravindcoco16@gmail.com>
2025-06-16 14:27:32 +0530

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT: Adjust this base path according to your GitHub Pages setup.
-  // If your site is at https://<username>.github.io/<repo-name>/, use base: '/<repo-name>/'
-  // If your site is at https://<username>.github.io/, (i.e. repo is <username>.github.io), use base: '/'
-  base: '/', // Example: '/your-repository-name/' or '/'
+  // Set base path for GitHub Pages deployment: https://username.github.io/reponame/
+  base: '/Portfolio/',
   build: {
     outDir: 'dist',
   }
